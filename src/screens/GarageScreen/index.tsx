6347bf12466d9c020f24698ebd30414a9aca31e8
Luizth25 <luizftp25@gmail.com>
2024-02-10 18:18:42 -0300

    first commit

---
+++ src/screens/GarageScreen/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+import { StatusBar } from "expo-status-bar";
+
+const GarageScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraBox}>
+        <View style={styles.triangleCorner}></View>
+        <View style={[styles.triangleCorner, styles.topRight]}></View>
+        <View style={[styles.triangleCorner, styles.bottomLeft]}></View>
+        <View style={[styles.triangleCorner, styles.bottomRight]}></View>
+      </View>
+      <StatusBar style="light" />
+    </View>
+  );
+};
+export default GarageScreen;
